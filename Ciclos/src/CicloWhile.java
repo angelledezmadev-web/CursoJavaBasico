74fd2895a26224f3f4d3afbc964df720217553ce
MarioAngelLedezma <angelledezma82@aragon.unam.mxgit config --global user.name MarioAngelLedezmagit clone https://[200~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~github.com/BeeXooCyberAI/beXoo-CS.gitgit config --global credential.helper storegit config --global user.email angelledezma82@aragon.unam.mx>
2026-09-01 13:07:33 -0600

    clilco

---
+++ Ciclos/src/CicloWhile.java
@@ -0,0 +1,12 @@
+public class CicloWhile {
+    public static void main(String[] args) {
+        System.out.println("*** Ciclo While");
+
+        int i = 1;
+
+        while (i <= 5){
+            System.out.println("contador = " + i);
+            i++;
+        }
+    }
+}
---
+++ Ciclos/src/Ciclos.java
@@ -0,0 +1,12 @@
+public class Ciclos {
+    public static void main(String[] args) {
+        System.out.println("*** Ciclo While");
+
+       int i = 1;
+
+       while (i <= 5){
+           System.out.println("i = " + i);
+           i++;
+       }
+    }
+}
